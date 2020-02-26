aaf9bff225bff8a304b41ea6a58b75b740deb222
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-26 09:46:22 -0800

    Updated for newer node, node-red and npm install.  Added 'sudo' to rsync of nginx config.

--- Install_EV4RV_base.sh
+++ Install_EV4RV_base.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 sudo apt update && sudo apt -y upgrade
-sudo apt -y install mosquitto mosquitto-clients nodered nginx git-core
+sudo apt -y install mosquitto mosquitto-clients nginx git-core
 sudo sed -i.bak -e 's/^persistence true$/persistence false/g' /etc/mosquitto/mosquitto.conf
 sudo systemctl restart mosquitto
-sudo npm install -g npm
+
+wget https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
+chmod 755 update-nodejs-and-nodered
+./update-nodejs-and-nodered --confirm-install --confirm-pi
 git clone https://github.com/linuxkidd/ev4rv.git
 rsync -axHASP ev4rv/node-red/ .node-red/
-rsync -axHASP ev4rv/nginx/ /etc/nginx/
+sudo rsync -axHASP ev4rv/nginx/ /etc/nginx/
 cd ~/.node-red/
 npm install node-red-dashboard node-red-node-emoncms
 sudo systemctl enable --now nodered
