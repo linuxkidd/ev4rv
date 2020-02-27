24563d6079ab8597637fb3a7dbcc8f928b7c1b1a
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-27 12:20:22 -0800

    More network message tweaking

--- ip_check_static.sh
+++ ip_check_static.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-defiface=$(/sbin/route -n | /usr/bin/awk '/^0.0.0.0/ { print $NF }')
+defiface=$(/sbin/route -n | /usr/bin/awk '/^0.0.0.0/ { print $NF; exit; }')
 myip=$(ip a s $defiface | awk '/inet / { split($2,a,"/"); print a[1]; exit; }')
 grep "^interface ${defiface}" /etc/dhcpcd.conf &> /dev/null
 RETVAL=$?
+
+echo -n '{"status":"'
 if [ $RETVAL -ne 0 ]; then
 	echo -n DHCP enabled
 else
@@ -11,7 +13,5 @@ else
 fi
 
 cat << EOF
-<table width='100%'>
-<tr><td><strong>URL:</strong></th><td align=right><a href="http://${myip}">http://${myip}</a></td></tr>
-</table>
+", "payload":"<table width='100%'><tr><td><strong>URL:</strong></th><td align=right><a href='http://${myip}'>http://${myip}</a></td></tr></table>"}
 EOF
