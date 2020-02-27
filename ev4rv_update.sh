39766ef31916574fff938f5d3cc2e2ad92d1d2ba
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-26 17:18:18 -0800

    Added MQTT feedback for post-upgrade restart

--- ev4rv_update.sh
+++ ev4rv_update.sh
@@ -11,6 +11,7 @@ fi
 
 unalias cp &> /dev/null
 echo Upgrade complete.  Restarting, please wait...
+(sleep 10 && /usr/bin/mosquitto_pub -t 'ev4rv/update' -m 'Update complete!') &
 sleep 5
 /usr/bin/curl --data-binary @/home/pi/ev4rv/node-red/flows.json -H 'Content-Type: application/json' -H 'Node-RED-Deployment-Type: full' http://localhost:1880/flows &> /dev/null
 exit 0
