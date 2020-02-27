dc020296e7abc45d99c77caba2ec8af06a9cb1b3
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-26 17:09:03 -0800

    Tweaked curl post

--- ev4rv_update.sh
+++ ev4rv_update.sh
@@ -12,5 +12,5 @@ fi
 unalias cp &> /dev/null
 echo Upgrade complete.  Restarting, please wait...
 sleep 5
-/usr/bin/curl --data-binary @~pi/ev4rv/node-red/flows.json -H 'Content-Type: application/json' -H 'Node-RED-Deployment-Type: full' http://localhost:1880/flows > /dev/null
+/usr/bin/curl --data-binary @/home/pi/ev4rv/node-red/flows.json -H 'Content-Type: application/json' -H 'Node-RED-Deployment-Type: full' http://localhost:1880/flows &> /dev/null
 exit 0
