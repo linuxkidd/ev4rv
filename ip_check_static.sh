c5aa5b4c209c655f276b9738616c6c46fccf36f6
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-27 12:05:58 -0800

    Removed hotspot link to minimize confusion

--- ip_check_static.sh
+++ ip_check_static.sh
@@ -9,11 +9,9 @@ if [ $RETVAL -ne 0 ]; then
 else
 	echo -n Static IP
 fi
-defgw=$(/sbin/route -n | awk '/^0.0.0.0/ { print $2 }')
 
 cat << EOF
 <table width='100%'>
 <tr><td><strong>URL:</strong></th><td align=right><a href="http://${myip}">http://${myip}</a></td></tr>
-<tr><td><strong>HotSpot URL:</strong></th><td align=right><a href="http://${defgw}">http://${defgw}</a></td></tr>
 </table>
 EOF
