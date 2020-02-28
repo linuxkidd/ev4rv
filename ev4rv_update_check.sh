a7f67b8d41fd6886d9dea3a26ed70fe21406a454
Michael J. Kidd <linuxkidd@gmail.com>
2020-02-28 10:36:57 -0800

    Fixed bad grep path

--- ev4rv_update_check.sh
+++ ev4rv_update_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/grep -c "$(/usr/bin/curl -H 'Cache-Control: no-cache' --silent https://raw.githubusercontent.com/linuxkidd/ev4rv/master/revision)" revision &> /dev/null
+/bin/grep -c "$(/usr/bin/curl -H 'Cache-Control: no-cache' --silent https://raw.githubusercontent.com/linuxkidd/ev4rv/master/revision)" ~pi/ev4rv/revision &> /dev/null
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
 	echo -n 1
